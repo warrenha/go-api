638ae288df273a1470d18688a762e86f3842f4dc
Warren Haslam <warren.haslam@gmail.com>
2026-06-14 13:41:03 +0100

    Added ping/pong to detect client disconnect, to the websocket

--- handlers/websocket.go
+++ handlers/websocket.go
@@ -9,6 +9,14 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+const (
+	// pingInterval is how often we send a ping to the client.
+	pingInterval = 30 * time.Second
+	// pongTimeout is how long we wait for a pong before considering the client dead.
+	// Must be greater than pingInterval.
+	pongTimeout = 60 * time.Second
+)
+
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
@@ -37,10 +45,21 @@ func sendMemoryStats(conn *websocket.Conn) error {
 	return conn.WriteMessage(websocket.TextMessage, responseBytes)
 }
 
-// listenForDisconnect starts a background goroutine to read from the connection
-// and returns a channel that will close when the client disconnects or an error occurs.
+// listenForDisconnect starts a background goroutine to read from the connection.
+// It sets up ping/pong handlers to detect dead connections and returns a channel
+// that closes when the client disconnects or stops responding to pings.
 func listenForDisconnect(conn *websocket.Conn) <-chan struct{} {
 	done := make(chan struct{})
+
+	// Set an initial read deadline. This will be extended each time a pong is received.
+	conn.SetReadDeadline(time.Now().Add(pongTimeout))
+
+	// When a pong is received, reset the read deadline, keeping the connection alive.
+	conn.SetPongHandler(func(string) error {
+		conn.SetReadDeadline(time.Now().Add(pongTimeout))
+		return nil
+	})
+
 	go func() {
 		defer close(done)
 		for {
@@ -51,6 +70,25 @@ func listenForDisconnect(conn *websocket.Conn) <-chan struct{} {
 		}
 	}()
 	return done
+}
+
+// startPingLoop sends periodic pings to the client to detect dead connections.
+// It stops when the done channel is closed.
+func startPingLoop(conn *websocket.Conn, done <-chan struct{}) {
+	ticker := time.NewTicker(pingInterval)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-done:
+			return
+		case <-ticker.C:
+			if err := conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+				log.Printf("failed to send ping: %v", err)
+				return
+			}
+		}
+	}
 }
 
 // broadcastMemoryStats starts a ticker and sends memory updates to the client
@@ -95,6 +133,9 @@ func Websocket(w http.ResponseWriter, r *http.Request) {
 
 	// Listen for client disconnection in the background
 	done := listenForDisconnect(conn)
+
+	// Send periodic pings to detect dead connections
+	go startPingLoop(conn, done)
 
 	// Broadcast memory stats to the client every 5 seconds until they disconnect
 	broadcastMemoryStats(conn, done, 5*time.Second)
